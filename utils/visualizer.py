735c75fabcaec95f6bf33b020420354a0ccf7fb9
mitrengamark <mitrengamark@edu.bme.hu>
2024-10-28 22:39:29 +0100

    #visualizer

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -1,36 +1,49 @@
-from utils.data_loader import process_audio
 import matplotlib.pyplot as plt
 import librosa as lb
-
-# Select a mel spectrogram to display
-  # Index for the specific spectrogram
-
-# Check if the selected mel spectrogram exists
-
+import numpy as np
+import subprocess
 
 
 class Visualizer():
     def __init__(self):
-        ...
+        pass
+        
         
     def show_sound(self, random_row):
         mel_spectogram = random_row['mel_spectrogram']
+        filename = random_row['filename']
+        filepath = f"data/train_audio/{filename}"
+        audio, sr = lb.load(filepath, sr=None)
+        duration = len(audio) / sr
+        
+        mel_spectogram = lb.feature.melspectrogram(y=audio, sr=sr)
+        mel_spectogram_db = lb.power_to_db(mel_spectogram, ref=np.max)
+
         if mel_spectogram is not None:
+            plt.ion()
             plt.figure(figsize=(10, 4))
-            lb.display.specshow(mel_spectogram, sr=32000, x_axis="time", y_axis="mel", fmax=16000)
+            lb.display.specshow(mel_spectogram_db, sr=sr, x_axis="time", y_axis="mel", fmax=sr / 2)
             plt.colorbar(format="%+2.0f dB")
-            plt.title("Mel Spectrogram (dB)")
+            plt.title(f"Mel Spectrogram (dB) - {filename}")
             plt.xlabel("Time")
             plt.ylabel("Frequency")
+            plt.xlim([0, duration])
             plt.tight_layout()
             plt.show()
+            plt.draw()
+            plt.pause(0.001)
         else:
             print("The selected spectrogram is None.")
 
     def play_sound(self, random_row):
-        pass
+        filename = random_row['filename']
+        filepath = f"data/train_audio/{filename}"
+        subprocess.run(['ffplay', '-nodisp', '-autoexit', filepath])
+        plt.ioff()
+        plt.show()
 
     def show_and_play(self, data_frame):
         random_row = data_frame.sample(n=1).iloc[0]
-        self.play_sound(random_row)
         self.show_sound(random_row)
+        self.play_sound(random_row)
+        print(random_row)
